a86f461e024a4362e883b82dbaa2e25d3349f856
wc <wc@qq.com>
2019-11-11 22:45:31 +0800

    wc提交了第一次的代码

---
+++ demo1/src/com/shop/aa/TestDemo1.java
@@ -0,0 +1,11 @@
+package com.shop.aa;
+
+/**
+ * 2019-11-11
+ * Loongson
+ **/
+public class TestDemo1 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaa");
+    }
+}
