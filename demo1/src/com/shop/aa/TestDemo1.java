aed87ff9ebc6ba1fe1882058ca407b0a8c0ff65d
wc <wc@qq.com>
2019-11-11 23:54:40 +0800

    李四也来啦......

--- demo1/src/com/shop/aa/TestDemo1.java
+++ demo1/src/com/shop/aa/TestDemo1.java
@@ -14,4 +14,11 @@ public class TestDemo1 {
         System.out.println("public void testaaa.......");
         System.out.println("李四来啦........");
     }
+
+    /**
+     * 又添加了第二个分支.......
+     */
+    public void testbrach(){
+        System.out.println("这是第二个分支.....");
+    }
 }
